105e1f6a30263b7b1543038159d0cd63f8a013c9
betelhem demsis <bettydemsis@gmail.com>
2024-08-26 19:06:03 +0300

    modified

--- backend/connection/urls.py
+++ backend/connection/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name='connection'
 
 urlpatterns=[
+    path('',views.inbox,name='inbox'),
+    path('',views.detail,name='detail'),
     path('new/<int:item_pk>/',views.new_connection,name='new_connection'),
 ]
--- backend/connection/views.py
+++ backend/connection/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render,get_object_or_404,redirect
+from django.contrib.auth.decorators import login_required
 from item.models import Item
 from .models import Conversation
 from .forms import ConversationForm
 # Create your views here.
 
+
+@login_required
 def new_connection(request,item_pk):
     item=get_object_or_404(Item,pk=item_pk)
 
@@ -29,3 +32,35 @@ def new_connection(request,item_pk):
         form=ConversationForm()
 
     return render(request,'connection/new_connection.html')
+
+@login_required
+def inbox(request):
+    conversations=Conversation.objects.filter(members__in=[request.user.id])
+
+    return render(request,'conversation/inbox.html',{
+        'conversations':conversations
+    })
+
+@login_required
+def detail(request,pk):
+    conversation=Conversation.objects.filter(members__in=[request.user.id]).get(pk=pk)
+
+    if request.method=='POST':
+        form=ConversationForm(request.POST)
+
+        if form.is_valid():
+            conversation_message=form.save(commit=False)
+            conversation_message.conversation=conversation
+            conversation_message.created_by=request.user
+            conversation_message.save()
+
+            conversation.save()
+            return redirect('connection:detail',pk=pk)
+        else:
+           form=ConversationForm()
+
+
+    return render(request,'conversation/detail.html',{
+        'conversation':conversation,
+        'form':form
+    })
